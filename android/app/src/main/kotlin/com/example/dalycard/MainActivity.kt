e3db506f1ec340841fa3cce845c1027edba935c9
Mohamed Ali Belkouri <medali.b@devnet.me>
2024-08-11 23:52:38 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/dalycard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dalycard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            backgroundColor: Colors.purple,
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 140,
+                  width: 140,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      "images/her.png",
+                    ),
+                  ),
+                ),
+                Text(
+                  "Daly's wife",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Hard work always pays off",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                  ),
+                ),
+                Text(
+                  "_____________________________",
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: 50,
+                    width: 1600,
+                    color: Colors.white,
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          color: Colors.purple,
+                          Icons.phone,
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Text(
+                          "+98 922 505 81690",
+                          style: TextStyle(
+                              color: Colors.purple,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        ),
+                      ],
+                    )),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                    height: 50,
+                    width: 1600,
+                    color: Colors.white,
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          color: Colors.purple,
+                          Icons.email,
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Text(
+                          "m.a.belkouri@gmail.com",
+                          style: TextStyle(
+                              color: Colors.purple,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    )),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                    height: 50,
+                    width: 1600,
+                    color: Colors.white,
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          color: Colors.purple,
+                          Icons.person,
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Text(
+                          "@DalyVolg",
+                          style: TextStyle(
+                              color: Colors.purple,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    )),
+              ],
+            )));
+  }
+}
